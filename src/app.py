08338ac1afd5d0930ec15450bf3143226a860671
kogcyc <kogcyc@gmail.com>
2024-01-26 17:06:31 -0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ secret_string = os.getenv('SUPER_SECRET')
 
 @app.route('/')
 def index():
-    return f'<br/><br/><br><span style="border-radius: 8px; padding: 40px; font-size: 2em; font-weight: 900; font-family: sans-serif; color: #fff; background-color: #79b;">{secret_string}</span>'
+    return f'<br/><br/><br><span style="border-radius: 8px; padding: 40px; font-size: 2em; font-weight: 400; font-family: sans-serif; color: #fff; background-color: #79b;">{secret_string}</span>'
 
 if __name__ == '__main__':
     app.run()
